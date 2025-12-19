8687576bcf6a6fc99e3742529f6e410df2f9249a
elbeeraa <ecarcelzam1@educacion.navarra.es>
2025-12-19 09:28:53 +0100

    Métodos añadidos

--- Examen_Elvira/src/Examen/Vehiculo.java
+++ Examen_Elvira/src/Examen/Vehiculo.java
@@ -1,5 +1,58 @@
 package Examen;
 
 public class Vehiculo {
+		private String matricula;
+		private String marca;
+		private String modelo;
+		private double preciohora;
+		
+		public Vehiculo(String matricula, String marca, String modelo, double preciohora) {
+			super();
+			this.matricula = matricula;
+			this.marca = marca;
+			this.modelo = modelo;
+			this.preciohora = preciohora;
+		}
 
+		public String getMatricula() {
+			return matricula;
+		}
+
+		public void setMatricula(String matricula) {
+			this.matricula = matricula;
+		}
+
+		public String getMarca() {
+			return marca;
+		}
+
+		public void setMarca(String marca) {
+			this.marca = marca;
+		}
+
+		public String getModelo() {
+			return modelo;
+		}
+
+		public void setModelo(String modelo) {
+			this.modelo = modelo;
+		}
+
+		public double getPreciohora() {
+			return preciohora;
+		}
+
+		public void setPreciohora(double preciohora) {
+			this.preciohora = preciohora;
+		}
+
+		@Override
+		public String toString() {
+			return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", preciohora="
+					+ preciohora + "]";
+		}
+		
+		
+		
+		
 }
