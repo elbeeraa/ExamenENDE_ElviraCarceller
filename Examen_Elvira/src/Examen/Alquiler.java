e0d2ff6135a02c999b68752a6728cf81676f6e1f
elbeeraa <ecarcelzam1@educacion.navarra.es>
2025-12-19 09:22:15 +0100

    Constructor añadido

--- Examen_Elvira/src/Examen/Alquiler.java
+++ Examen_Elvira/src/Examen/Alquiler.java
@@ -3,8 +3,7 @@ package Examen;
 public class Alquiler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- Examen_Elvira/src/Examen/Vehiculo.java
+++ Examen_Elvira/src/Examen/Vehiculo.java
@@ -1,5 +1,17 @@
 package Examen;
 
 public class Vehiculo {
-
+		private String matricula;
+		private String marca;
+		private String modelo;
+		private double preciohora;
+		
+		public Vehiculo(String matricula, String marca, String modelo, double preciohora) {
+			super();
+			this.matricula = matricula;
+			this.marca = marca;
+			this.modelo = modelo;
+			this.preciohora = preciohora;
+		}
+		
 }
